d5a330f787a07b491850b71fc289c81bc208b854
Isabelle-123 <isacas031@gmail.com>
2020-06-26 13:30:37 +0200

    new start

--- src/context/types.js
+++
@@ -1,8 +0,0 @@
-//change state with these types
-export const SEARCH_USERS = 'SEARCH_USERS'
-export const GET_USER = 'GET_USERS'
-export const CLEAR_USERS = 'CLEAR_USERS'
-export const SET_LOADING = 'SET_LOADING'
-export const SET_ALERT = 'SET_ALERT'
-export const REMOVE_ALERT = 'REMOVE_ALERT'
-export const GET_REPOS = 'GET_REPOS'
