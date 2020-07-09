039988fe2f558dbc3e0d8a8092dd110cd0c486fe
Isabelle-123 <isacas031@gmail.com>
2020-07-09 13:41:58 +0200

    added repoitem componenet

---
+++ src/components/repos/RepoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RepoItem = () => {
+  return (
+    <div className='card'>
+      <h3>
+        <a href={RepoItem.html_url}>{repo.name}</a>
+      </h3>
+    </div>
+  )
+}
+
+export default RepoItem
--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import RepoItem from './RepoItem'
 
 const Repos = ({ repos }) => {
   return repos.map((repo) => <RepoItem repo={repo} key={repo.id} />)
